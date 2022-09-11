ca63350223234e396e32fb985e9fdf82edc0e364
abdul gilani <abdul.gilani@gmail.com>
2022-09-11 19:58:28 +0100

    worldcoin

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -3,36 +3,22 @@ pragma solidity ^0.8.13;
 
 import { ByteHasher } from './helpers/ByteHasher.sol';
 import { IWorldID } from './interfaces/IWorldID.sol';
+import "hardhat/console.sol";
 
 contract Contract {
     using ByteHasher for bytes;
 
-    ///////////////////////////////////////////////////////////////////////////////
-    ///                                  ERRORS                                ///
-    //////////////////////////////////////////////////////////////////////////////
-
-    /// @notice Thrown when attempting to reuse a nullifier
     error InvalidNullifier();
-
-    /// @dev The WorldID instance that will be used for verifying proofs
     IWorldID internal immutable worldId;
-
-    /// @dev The WorldID group ID (1)
     uint256 internal immutable groupId = 1;
-
-    /// @dev Whether a nullifier hash has been used already. Used to prevent double-signaling
     mapping(uint256 => bool) internal nullifierHashes;
 
-    /// @param _worldId The WorldID instance that will verify the proofs
+    event ProofVerified();
+
     constructor(IWorldID _worldId) {
         worldId = _worldId;
     }
 
-    /// @param input User's input, used as the signal. Could be something else! (see README)
-    /// @param root The of the Merkle tree, returned by the SDK.
-    /// @param nullifierHash The nullifier for this proof, preventing double signaling, returned by the SDK.
-    /// @param proof The zero knowledge proof that demostrates the claimer is registered with World ID, returned by the SDK.
-    /// @dev Feel free to rename this method however you want! We've used `claim`, `verify` or `execute` in the past.
     function verifyAndExecute(
         address input,
         uint256 root,
@@ -55,6 +41,7 @@ contract Contract {
         // finally, we record they've done this, so they can't do it again (proof of uniqueness)
         nullifierHashes[nullifierHash] = true;
 
-        // your logic here, make sure to emit some kind of event afterwards!
+        // logic
+        // Mint profile nft
     }
 }
